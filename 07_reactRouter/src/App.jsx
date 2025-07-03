be1e85c414945ef0849165eceb3849fc2d487985
Dhruvichavda02 <dhruvichavda20@gmail.com>
2025-07-03 09:38:16 +0530

    React Router

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Footer/>
+    
+    </>
+  )
+}
+
+export default App
